19396262055bd5196f6cc669f737b332074d39f8
shabista.sarwer@gmail.com <shabista.sarwer@gmail.com>
2020-01-07 16:49:47 +0530

    updated

--- admin/productList.php
+++ admin/productList.php
@@ -23,7 +23,7 @@ $limit = 10;
 $record_index= ($page-1) * $limit; 
 
 $select = $conn->prepare("SELECT pi.id,pi.status,pi.name, pi.reference, pi.price,pc.category FROM product_category as pc INNER JOIN product_item as pi on pc.id=pi.c_id 
-        order by pi.id DESC ");
+       ".$term." order by pi.id DESC LIMIT $record_index,$limit");
 $select->execute();
 $row_count= $select->rowCount();
 $res = $select->fetch(PDO::FETCH_BOTH);
